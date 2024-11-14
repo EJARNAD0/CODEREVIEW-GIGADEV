afeacca9a0c1c882bb48496592e8135fb8448124
johnarmor <100022397+johnarmor@users.noreply.github.com>
2024-11-14 19:21:45 +0800

    Update db_connection.php

--- WEB-ADMIN/users-module/db_connection.php
+++ WEB-ADMIN/users-module/db_connection.php
@@ -2,10 +2,10 @@
 // db_connection.php
 
 // Database configuration
-$servername = "localhost:3306"; // Your server name, usually localhost
-$username = "wanderej_plato"; // Your database username
-$password = "Demesa123"; // Your database password
-$dbname = "wanderej_plato"; // Replace with your database name
+$servername = ""; // Your server name, usually localhost
+$username = ""; // Your database username
+$password = ""; // Your database password
+$dbname = ""; // Replace with your database name
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
